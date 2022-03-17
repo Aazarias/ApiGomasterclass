d179773e8f379ef490cc0470fbdbb370d2bf08c1
Aazarias <38279952+Aazarias@users.noreply.github.com>
2022-03-17 04:12:03 +0100

    Correction release date

--- model/games.go
+++ model/games.go
@@ -5,5 +5,5 @@ type Games struct {
 	Name        string `json:"games_name"`
 	Mark        string `json:"mark"`
 	Price       string `json:"price"`
-	RealaseDate string `json:"relase_date"`
+	RealaseDate string `json:"release_date"`
 }
