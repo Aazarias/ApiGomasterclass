86857eb035b66ac4d957c5b65d8980e903583186
Aazarias <38279952+Aazarias@users.noreply.github.com>
2022-03-17 02:46:14 +0100

    Update main.go

--- main.go
+++ main.go
@@ -10,9 +10,9 @@ func main() {
 	r := gin.Default()
 	db := moke.New()
 	s := service.New(db)
-	r.GET("/heroes/:id", s.GetGames)
-	r.POST("/heroes", s.CreateGames)
-	r.GET("/heroes", s.GetAllGames)
-	r.DELETE("/heroes/:id", s.DeleteGames)
+	r.GET("/games:id", s.GetGames)
+	r.POST("/games", s.CreateGames)
+	r.GET("/games", s.GetAllGames)
+	r.DELETE("/games/:id", s.DeleteGames)
 	r.Run(":8081")
 }
